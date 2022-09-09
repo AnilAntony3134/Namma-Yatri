e81b868f768e438c4f1eff086b08d0eede4c08bd
Anil Antony <anil.antony@settlin.in>
2022-09-09 22:53:42 +0530

    navbar v1

--- imports/ui/Home.tsx
+++ imports/ui/Home.tsx
@@ -1,10 +1,17 @@
+import { styled } from '@mui/material';
 import React, { useState } from 'react';
-import {NavBar} from './client/NavBar';
+import NavBar from './client/NavBar';
 
+const StyledDiv = styled('div')(() => ({
+  display: 'flex',
+  padding: '0 150px 0 150px',
+  justifyContent: 'center',
+  alignItems: 'center'
+}))
 export const Home = () => {
   return (
-    <div>
+    <StyledDiv>
       <NavBar />
-    </div>
+    </StyledDiv>
   );
 };
--- imports/ui/client/NavBar.tsx
+++ imports/ui/client/NavBar.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { styled } from "@mui/material";
+import { Grid } from "@mui/material";
+import React from "react";
 
-export const NavBar = () => {
+const PREFIX = "Navbar";
+
+const classes = {
+  navLeft: `${PREFIX}-navLeft`,
+  title: `${PREFIX}-title`
+};
+
+const StyledGrid = styled(Grid)(() => ({
+  [`&.${classes.navLeft}`]: {
+    display: "flex",
+    backgroundColor: "green",
+    flexDirection: "column",
+  },
+  [`&.${classes.title}`]: {
+    padding: '10px',
+    color: 'blue'
+  }
+}));
+
+
+const navItem = [{item: 'Guide'},{item: 'New'},{item: 'Load'}];
+
+const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <StyledGrid container justifyContent="space-between" padding={1}>
+      <Grid item>
+        <Grid container className={classes.navLeft}>
+          <Grid className={classes.title}>Uno Counter</Grid>
+          <Grid item>Guide</Grid>
+          <Grid item>New</Grid>
+          <Grid item>Load</Grid>
+        </Grid>
+      </Grid>
+      <Grid item>User</Grid>
+    </StyledGrid>
+  );
+};
+
+export default NavBar;
