b0bba71cd9c8b4420b065d96aa15d3ff99321bc7
Anil Antony <anil.antony@settlin.in>
2022-09-10 13:16:49 +0530

    fix: added changes

--- imports/ui/client/MainTable.tsx
+++ imports/ui/client/MainTable.tsx
@@ -6,7 +6,7 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Paper from '@mui/material/Paper'; 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
