e14ccc249cdddcea3cd5cbb4234d91f4ec794a08
Anil Antony <anilantony3134@gmail.com>
2023-04-05 08:18:52 +0530

    fix: added icon

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from '../firebase'
 import Map from '../components/Map';
+import Location from '../components/Location';
 
 export default function Home() {
   const [user, setUser] = useState(null)
@@ -17,41 +18,26 @@ export default function Home() {
           name: user.displayName,
           photoUrl:user.photoURL,
         })
-      } else {
-        setUser(null)
-        router.push('/login')
-      }
+      } 
+      // else {
+      //   setUser(null)
+      //   router.push('/login')
+      // }
     })
   })
 
   return (
     <Wrapper>
+      <Location />
       <Map pickup={[]} dropoff={[]} />
       <ActionItems>
-        <Header>
-          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg"/>
-          <Profile>
-            <Name>{user && user.name}</Name>
-            <UserImage src={user && user.photoUrl} onClick={() => signOut(auth)}/>
-          </Profile>
-        </Header>
         <ActionButtons>
           <Link href="/search">
             <ActionButton>
-              <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png"/>
-              Ride
+              <InputButton> &#x21B3; &nbsp; Where to?</InputButton>
             </ActionButton>
           </Link>
-          <ActionButton>
-          <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png"/>
-            Wheel
-          </ActionButton>
-          <ActionButton>
-          <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png"/>
-            Reserve
-          </ActionButton>
-        </ActionButtons>
-        <InputButton>Where to?</InputButton>
+          </ActionButtons>
       </ActionItems>
     </Wrapper>
   )
@@ -62,33 +48,14 @@ flex flex-col h-screen
 `;
 
 const ActionItems = tw.div`
-flex-1 p-4
+flex-3 p-6
 `;
-
-const Header=tw.div`
-flex justify-between items-center
-`
-const UberLogo=tw.img`
-h-28 
-`
-const Profile=tw.div`
-flex items-center
-`
-const Name=tw.div`
-mr-4 w-20 text-sm
-`
-const UserImage=tw.img`
-h-12 w-12 rounded-full border-gray-200 p-px cursor-pointer
-`
 const ActionButtons=tw.div`
 flex 
 `
 const ActionButton=tw.div`
-bg-gray-200 flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl
-`
-const ActionButtonImage=tw.img`
-h-3/5
+flex-1 m-1 h-18 items-center flex-col justify-center transform hover:scale-105 transition rounded-md
 `
 const InputButton=tw.div`
-h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
+h-16 bg-gray-800 text-yellow-400 text-md px-4 flex items-center rounded-md font-medium tracking-wide
 `
